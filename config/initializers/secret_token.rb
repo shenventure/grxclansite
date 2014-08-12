e3352f5eb53e1dc78ffd0da14c3e3cd6ea98166b
Adrienne Shen <pouringhoney@gmail.com>
2014-08-11 17:16:48 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Grxclan::Application.config.secret_key_base = 'b16e01fe1a4ba30e42cee4892c95ebc09f5d9875a06f15973f321f0ec8c957fe29637ae36c0d1f93daaad18063632066b3414d544fcfeffbe7b72721d84399d3'
