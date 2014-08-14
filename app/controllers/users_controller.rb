cddc2f57b9c46635297988154516d595a53e1822
Adrienne Shen <pouringhoney@gmail.com>
2014-08-13 17:12:52 -0700

    Fix Gamertag to save on Sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,18 @@ class UsersController < ApplicationController
   end
 
   def new
+  	@user = User.new  	
   end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      sign_in @user	
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end    	
 
   def show
   end
